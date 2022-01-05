4b1b9eb0f6289ad7c6ea05868d43a16c471860aa
Aakash Patil <patil.aa@northeastern.edu>
2022-01-05 02:20:49 -0500

    changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./api/app');
-const port = 5010
 
 //backend project listens on port 3000
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 })
